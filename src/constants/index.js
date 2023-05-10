bcacfb81442731942ad1e45fd8b14416be69f304
thiagojava <dutrainformatica5@gmail.com>
2023-05-10 02:07:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/constants/index.js
@@ -0,0 +1,215 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "Sobre",
+  },
+  {
+    id: "work",
+    title: "Trabalho",
+  },
+  {
+    id: "contact",
+    title: "Contato",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Node Developer",
+    icon: backend,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+ 
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+ 
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "Grupo Boticário",
+    icon: "starbucks",
+    iconBg: "#383E56",
+    date: "March 2022 - Até o momento",
+    points: [
+      "Desenvolvimento e manutenção de aplicativos da web usando React.js, Node.js e outras tecnologias relacionadas.Colaboração com equipes multifuncionais, incluindo designers, gerentes de produto e outros desenvolvedores, para criar produtos de alta qualidade.Implementação de design responsivo e garantia de compatibilidade entre navegadores.Participação em revisões de código e fornecimento de feedback construtivo para outros desenvolvedores.",
+    ],
+  },
+  {
+    title: "Developer",
+    company_name: "Bellinati Perez",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Abr 2022",
+    points: [
+      "Desenvolvimento de novas funcionalidades em VB.NET, resoluções de bugs e problemas relacionados a código e performances.",
+    ],
+  }
+];
+
+const testimonials = [
+  {
+    testimonial:"Trabalho de excelente qualidade e dentro do prazo. Atendeu todas expectativas",
+    name: "Bruno Lopes",
+    designation: "Gerente",
+    company: "AM",
+  },
+  {
+    testimonial:"Excelente, recomendo a todos",
+    name: "Gabriel Gomes",
+    designation: "Senior Developer",
+    company: "Bellinati Perez",
+  },
+  {
+    testimonial:"Muito ágil e atencioso",
+    name: "Lisa Perez",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
